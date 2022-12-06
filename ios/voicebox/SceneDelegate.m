c89d2232467700e619e3a8963cbc1d907b1521da
scosman <scosman@users.noreply.github.com>
2022-12-05 23:04:48 -0500

    Remove storyboards, launch view controller directly.
    Cleanup .gitignore for DS_Store and user data

--- ios/voicebox/SceneDelegate.m
+++ ios/voicebox/SceneDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "SceneDelegate.h"
+#import "ViewController.h"
 
 @interface SceneDelegate ()
 
@@ -15,8 +16,16 @@
 
 
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+    
+    if (![scene isKindOfClass:[UIWindowScene class]]) {
+        return;
+    }
+    UIWindowScene* windowScene = (UIWindowScene*)scene;
+    UIWindow* window = [[UIWindow alloc] initWithWindowScene:windowScene];
+    window.rootViewController = [[ViewController alloc] init];
+    self.window = window;
+    [window makeKeyAndVisible];
+    
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 }
 
