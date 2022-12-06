ab5bb9d6087fd906882e4e65ce6e7170249e7546
scosman <scosman@users.noreply.github.com>
2022-12-06 16:31:39 -0500

    Add AppSecrets.h and add to gitignore

---
+++ ios/voicebox/Util/AppSecrets.h
@@ -0,0 +1,13 @@
+//
+//  AppSecrets.h
+//  voicebox
+//
+//  Created by Steve Cosman on 2022-12-06.
+//
+
+#ifndef AppSecrets_h
+#define AppSecrets_h
+
+#define OPEN_API_KEY @"PUT YOUR OPENAPI.COM KEY HERE"
+
+#endif /* AppSecrets_h */
