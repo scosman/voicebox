0549f81b58082d463c0bd4a16e9b12cd7970e9bf
scosman <scosman@users.noreply.github.com>
2023-03-01 13:03:08 -0500

    Refactor the listener out into it's own class, separate from the VC. The VC is now a nice pure VC!
    Fixed the memory leak where each time we listened, we initialized the whisper model.
    
    New utility class still needs work
     - more idiomatic objC
     - remove unneeded code (transcription modes)
     - pre-warm model on app start for faster startup

---
+++ ios/voicebox/Whisper/VBAudioListener.h
@@ -0,0 +1,27 @@
+//
+//  VBAudioListener.h
+//  voicebox
+//
+//  Created by Steve Cosman on 2023-03-01.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol VBAudioListenerDelegate <NSObject>
+@required
+-(void) stateUpdate:(bool)running segments:(nullable NSArray<NSString*>*)segments;
+@end
+
+@interface VBAudioListener : NSObject
+
++(VBAudioListener*) sharedInstance;
+
+-(void) registerDelegate:(id <VBAudioListenerDelegate>)delegate;
+-(void) startListening;
+-(void)stopCapturing;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/voicebox/Whisper/VBAudioListener.m
@@ -0,0 +1,335 @@
+//
+//  VBAudioListener.m
+//  voicebox
+//
+//  Created by Steve Cosman on 2023-03-01.
+//  Modified from example available here with MIT licence: https://github.com/ggerganov/whisper.cpp
+//
+
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioQueue.h>
+
+#import "VBAudioListener.h"
+#import "whisper.h"
+
+#define NUM_BYTES_PER_BUFFER 16 * 1024
+
+#define NUM_BUFFERS 3
+#define MAX_AUDIO_SEC 120
+#define SAMPLE_RATE 16000
+
+struct whisper_context;
+
+// TODO idiomatic ObjC
+typedef struct
+{
+    int ggwaveId;
+    bool isCapturing;
+    bool isTranscribing;
+    bool isRealtime;
+
+    AudioQueueRef queue;
+    AudioStreamBasicDescription dataFormat;
+    AudioQueueBufferRef buffers[NUM_BUFFERS];
+
+    int n_samples;
+    int16_t* audioBufferI16;
+    float* audioBufferF32;
+
+    struct whisper_context* ctx;
+
+    void* vc;
+} StateInp;
+
+
+@interface VBAudioListener () {
+    StateInp stateInp;
+}
+
+@property (nonatomic, weak) id <VBAudioListenerDelegate> delegate;
+
+@end
+
+@implementation VBAudioListener
+
++ (VBAudioListener*)sharedInstance
+{
+    static VBAudioListener *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+
+    return sharedInstance;
+}
+
+-(instancetype)init {
+    self = [super init];
+    if (self) {
+        // whisper.cpp initialization
+        
+        /* From rough experimentation, the base model seems to work well enough, so sticking to that
+         * for now. The small model works too, with similar CPU but double the memory, and slower
+         * processing time (CPU probably isn't telling whole story).
+         * Next step to test on a device. Not sure if we're getting ANE acceleration, and how an iPad's
+         * microphone comares to macbook.
+         *
+         * If you want to play with this, add the models to "Copy Bundle Resources" step of build.
+         */
+        // load the "base" whisper model
+        NSString* modelPath = [[NSBundle mainBundle] pathForResource:@"ggml-base.en" ofType:@"bin"];
+        // NSString* modelPath = [[NSBundle mainBundle] pathForResource:@"ggml-small.en" ofType:@"bin"];
+
+        // check if the model exists
+        if (![[NSFileManager defaultManager] fileExistsAtPath:modelPath]) {
+            NSLog(@"Model file not found");
+        }
+
+        NSLog(@"Loading model from %@", modelPath);
+
+        // create ggml context
+        stateInp.ctx = whisper_init([modelPath UTF8String]);
+
+        // check if the model was loaded successfully
+        if (stateInp.ctx == NULL) {
+            NSLog(@"Failed to load model");
+        }
+    }
+    return self;
+}
+
+-(void) registerDelegate:(id <VBAudioListenerDelegate>)delegate {
+    // TODO NSNotification Center
+    self.delegate = delegate;
+    // TODO send latest state on register to just this delegate
+}
+
+- (void)setupAudioFormat:(AudioStreamBasicDescription*)format
+{
+    format->mSampleRate = WHISPER_SAMPLE_RATE;
+    format->mFormatID = kAudioFormatLinearPCM;
+    format->mFramesPerPacket = 1;
+    format->mChannelsPerFrame = 1;
+    format->mBytesPerFrame = 2;
+    format->mBytesPerPacket = 2;
+    format->mBitsPerChannel = 16;
+    format->mReserved = 0;
+    format->mFormatFlags = kLinearPCMFormatFlagIsSignedInteger;
+}
+
+- (void)startListening
+{
+    if (stateInp.ctx == NULL) {
+        // initialization failed
+        [_delegate stateUpdate:false segments:nil];
+        return;
+    }
+    
+    // initialize audio format and buffers
+    [self setupAudioFormat:&stateInp.dataFormat];
+
+    stateInp.n_samples = 0;
+    stateInp.audioBufferI16 = malloc(MAX_AUDIO_SEC * SAMPLE_RATE * sizeof(int16_t));
+    stateInp.audioBufferF32 = malloc(MAX_AUDIO_SEC * SAMPLE_RATE * sizeof(float));
+
+    stateInp.isTranscribing = false;
+    stateInp.isRealtime = true;
+    
+    // TODO - might already be capturing!
+    [self startCapturing];
+}
+
+- (void)stopCapturing
+{
+    // TODO test dispatch
+    __block VBAudioListener* blockSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"Stop capturing");
+        
+        blockSelf->stateInp.isCapturing = false;
+        
+        AudioQueueStop(blockSelf->stateInp.queue, true);
+        for (int i = 0; i < NUM_BUFFERS; i++) {
+            AudioQueueFreeBuffer(blockSelf->stateInp.queue, blockSelf->stateInp.buffers[i]);
+        }
+        
+        AudioQueueDispose(blockSelf->stateInp.queue, true);
+    });
+}
+
+- (void)startCapturing
+{
+    __block VBAudioListener* blockSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        // initiate audio capturing
+        NSLog(@"Start capturing");
+        
+        blockSelf->stateInp.n_samples = 0;
+        blockSelf->stateInp.vc = (__bridge void*)(self);
+        
+        OSStatus status = AudioQueueNewInput(&blockSelf->stateInp.dataFormat,
+                                             AudioInputCallback,
+                                             &blockSelf->stateInp,
+                                             CFRunLoopGetCurrent(),
+                                             kCFRunLoopCommonModes,
+                                             0,
+                                             &blockSelf->stateInp.queue);
+        
+        if (status != 0) {
+            return;
+        }
+        for (int i = 0; i < NUM_BUFFERS; i++) {
+            AudioQueueAllocateBuffer(blockSelf->stateInp.queue, NUM_BYTES_PER_BUFFER, &blockSelf->stateInp.buffers[i]);
+            AudioQueueEnqueueBuffer(blockSelf->stateInp.queue, blockSelf->stateInp.buffers[i], 0, NULL);
+        }
+        
+        blockSelf->stateInp.isCapturing = true;
+    
+        status = AudioQueueStart(blockSelf->stateInp.queue, NULL);
+        
+        if (status != 0) {
+            [self stopCapturing];
+        }
+        bool running = status == 0;
+        [blockSelf.delegate stateUpdate:running segments:nil];
+    });
+}
+
+// TODO no sender
+// Start needs to ser
+- (IBAction)onRealtime:(id)sender
+{
+    stateInp.isRealtime = !stateInp.isRealtime;
+
+    NSLog(@"Realtime: %@", stateInp.isRealtime ? @"ON" : @"OFF");
+}
+
+// TODO Delete this
+- (IBAction)onTranscribe:(id)sender
+{
+    if (stateInp.isTranscribing) {
+        return;
+    }
+
+    NSLog(@"Processing %d samples", stateInp.n_samples);
+
+    stateInp.isTranscribing = true;
+
+    // dispatch the model to a background thread
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // process captured audio
+        // convert I16 to F32
+        for (int i = 0; i < self->stateInp.n_samples; i++) {
+            self->stateInp.audioBufferF32[i] = (float)self->stateInp.audioBufferI16[i] / 32768.0f;
+        }
+
+        // run the model
+        struct whisper_full_params params = whisper_full_default_params(WHISPER_SAMPLING_GREEDY);
+
+        // get maximum number of threads on this device (max 8)
+        const int max_threads = MIN(8, (int)[[NSProcessInfo processInfo] processorCount]);
+
+        params.print_realtime = true;
+        params.print_progress = false;
+        params.print_timestamps = true;
+        params.print_special = false;
+        params.translate = false;
+        params.language = "en";
+        params.n_threads = max_threads;
+        params.offset_ms = 0;
+        params.no_context = true;
+        params.single_segment = self->stateInp.isRealtime;
+
+        CFTimeInterval startTime = CACurrentMediaTime();
+
+        whisper_reset_timings(self->stateInp.ctx);
+
+        if (whisper_full(self->stateInp.ctx, params, self->stateInp.audioBufferF32, self->stateInp.n_samples) != 0) {
+            NSLog(@"Failed to run the model");
+            [_delegate stateUpdate:false segments:nil];
+
+            return;
+        }
+
+        whisper_print_timings(self->stateInp.ctx);
+
+        CFTimeInterval endTime = CACurrentMediaTime();
+
+        NSLog(@"\nProcessing time: %5.3f, on %d threads", endTime - startTime, params.n_threads);
+
+        int n_segments = whisper_full_n_segments(self->stateInp.ctx);
+        NSMutableArray<NSString*>* segments = [[NSMutableArray alloc] initWithCapacity:n_segments];
+        for (int i = 0; i < n_segments; i++) {
+            const char* text_cur = whisper_full_get_segment_text(self->stateInp.ctx, i);
+            [segments addObject:[NSString stringWithUTF8String:text_cur]];
+        }
+
+        const float tRecording = (float)self->stateInp.n_samples / (float)self->stateInp.dataFormat.mSampleRate;
+
+        // log processing time
+        NSLog(@"[recording time:  %5.3f s] [processing time: %5.3f s]", tRecording, endTime - startTime);
+
+        // TODO remove dispatch
+        // dispatch the result to the main thread
+        //dispatch_async(dispatch_get_main_queue(), ^{
+            //weakSelf.closedCaptioningLabel.text = result;
+            [_delegate stateUpdate:true segments:segments];
+            self->stateInp.isTranscribing = false;
+        //});
+    });
+}
+
+
+// TODO needed?
+
+//
+// Callback implementation
+//
+void AudioInputCallback(void* inUserData,
+    AudioQueueRef inAQ,
+    AudioQueueBufferRef inBuffer,
+    const AudioTimeStamp* inStartTime,
+    UInt32 inNumberPacketDescriptions,
+    const AudioStreamPacketDescription* inPacketDescs)
+{
+    StateInp* stateInp = (StateInp*)inUserData;
+
+    if (!stateInp->isCapturing) {
+        NSLog(@"Not capturing, ignoring audio");
+        return;
+    }
+
+    const int n = inBuffer->mAudioDataByteSize / 2;
+
+    NSLog(@"Captured %d new samples", n);
+
+    if (stateInp->n_samples + n > MAX_AUDIO_SEC * SAMPLE_RATE) {
+        NSLog(@"Too much audio data, ignoring");
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            VBAudioListener* listener = (__bridge VBAudioListener*)(stateInp->vc);
+            [listener stopCapturing];
+        });
+
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        stateInp->audioBufferI16[stateInp->n_samples + i] = ((short*)inBuffer->mAudioData)[i];
+    }
+
+    stateInp->n_samples += n;
+
+    // put the buffer back in the queue
+    AudioQueueEnqueueBuffer(stateInp->queue, inBuffer, 0, NULL);
+
+    if (stateInp->isRealtime) {
+        // dipatch onTranscribe() to the main thread
+        dispatch_async(dispatch_get_main_queue(), ^{
+            VBAudioListener* listener = (__bridge VBAudioListener*)(stateInp->vc);
+            [listener onTranscribe:nil];
+        });
+    }
+}
+
+@end
