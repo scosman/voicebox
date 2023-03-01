53112499954ff599d3e5068cc547354d0759cf6f
scosman <scosman@users.noreply.github.com>
2023-03-01 13:05:16 -0500

    Fix UI button initializers

--- ios/voicebox/UI/VBButton.m
+++ ios/voicebox/UI/VBButton.m
@@ -20,44 +20,44 @@
 - (instancetype)initLargeSymbolButtonWithSystemImageNamed:(NSString*)systemImageName andTitle:(NSString*)title
 {
     self = [super init];
+    if (self) {
+        UIButtonConfiguration* config = UIButtonConfiguration.filledButtonConfiguration;
+        config.image = [UIImage systemImageNamed:systemImageName withConfiguration:[UIImageSymbolConfiguration configurationWithPointSize:28.0]];
+        config.imagePlacement = NSDirectionalRectEdgeTop;
+        config.imagePadding = 8.0;
+        config.attributedTitle = [[NSAttributedString alloc] initWithString:title attributes:@{ NSFontAttributeName : [UIFont systemFontOfSize:MAX([UIFont labelFontSize], 24.0)] }];
+        self.configuration = config;
 
-    UIButtonConfiguration* config = UIButtonConfiguration.filledButtonConfiguration;
-    config.image = [UIImage systemImageNamed:systemImageName withConfiguration:[UIImageSymbolConfiguration configurationWithPointSize:28.0]];
-    config.imagePlacement = NSDirectionalRectEdgeTop;
-    config.imagePadding = 8.0;
-    config.attributedTitle = [[NSAttributedString alloc] initWithString:title attributes:@{ NSFontAttributeName : [UIFont systemFontOfSize:MAX([UIFont labelFontSize], 24.0)] }];
-    self.configuration = config;
-
-    [self setupStandardActionButtonColor];
-
+        [self setupStandardActionButtonColor];
+    }
     return self;
 }
 
 - (instancetype)initOptionButtonWithTitle:(NSString*)title
 {
     self = [super init];
+    if (self) {
+        UIButtonConfiguration* config = UIButtonConfiguration.filledButtonConfiguration;
+        config.attributedTitle = [[NSAttributedString alloc] initWithString:title attributes:@{ NSFontAttributeName : [UIFont systemFontOfSize:MAX([UIFont labelFontSize], 24.0)] }];
+        config.contentInsets = NSDirectionalEdgeInsetsMake(16, 16, 16, 16);
+        config.background.backgroundColor = ACTION_BUTTON_UICOLOR;
+        self.configuration = config;
 
-    UIButtonConfiguration* config = UIButtonConfiguration.filledButtonConfiguration;
-    config.attributedTitle = [[NSAttributedString alloc] initWithString:title attributes:@{ NSFontAttributeName : [UIFont systemFontOfSize:MAX([UIFont labelFontSize], 24.0)] }];
-    config.contentInsets = NSDirectionalEdgeInsetsMake(16, 16, 16, 16);
-    config.background.backgroundColor = ACTION_BUTTON_UICOLOR;
-    self.configuration = config;
-
-    [self setupStandardActionButtonColor];
-
+        [self setupStandardActionButtonColor];
+    }
     return self;
 }
 
 - (instancetype)initOptionCancelButton
 {
     self = [super init];
-
-    UIButtonConfiguration* config = UIButtonConfiguration.grayButtonConfiguration;
-    config.attributedTitle = [[NSAttributedString alloc] initWithString:@"Cancel" attributes:@{ NSFontAttributeName : [UIFont systemFontOfSize:MAX([UIFont labelFontSize], 24.0)] }];
-    config.contentInsets = NSDirectionalEdgeInsetsMake(16, 16, 16, 16);
-    config.baseForegroundColor = ACTION_BUTTON_UICOLOR;
-    self.configuration = config;
-
+    if (self) {
+        UIButtonConfiguration* config = UIButtonConfiguration.grayButtonConfiguration;
+        config.attributedTitle = [[NSAttributedString alloc] initWithString:@"Cancel" attributes:@{ NSFontAttributeName : [UIFont systemFontOfSize:MAX([UIFont labelFontSize], 24.0)] }];
+        config.contentInsets = NSDirectionalEdgeInsetsMake(16, 16, 16, 16);
+        config.baseForegroundColor = ACTION_BUTTON_UICOLOR;
+        self.configuration = config;
+    }
     return self;
 }
 
